ab04654eb32d79068f45398c6ea0dcea8c7f61c1
Aurtherle <113518431+Aurtherle@users.noreply.github.com>
2024-04-05 03:20:22 +0300

    Update gpt4.js

--- plugins/gpt4.js
+++ plugins/gpt4.js
@@ -7,16 +7,16 @@ if (args.length >= 1) {
 text = args.slice(0).join(' ')
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text
-} else return conn.reply(m.chat, `*🎌 Este comando genera imagenes apartir de textos*\n\nEjemplo, !dalle Estrella naciente`, m, fake, )
+} else return conn.reply(m.chat, `*🎌 Este comando genera imagenes apartir de textos*\n\nEjemplo, !dalle Estrella naciente`, m)
 
 try {
 
-conn.reply(m.chat, '⏰ Espere un momento', m, fake, )
+conn.reply(m.chat, '⏰ Espere un momento', m)
 await Draw(text).then((img) => {
 conn.sendFile(m.chat, img, text, `*🍧 Resultado de* ${text}\n\n`, m)
 })
 } catch (e) {
-return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
+return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m)
 }
 
 }
